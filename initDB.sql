1b88ddede80b2125049347bb32f0bec7d95fc423
Mario Saponara <mario.saponara@ext.grupposandonato.it>
2018-07-11 10:39:55 +0200

    db

--- initDB.sql
+++ initDB.sql
@@ -1,10 +1,51 @@
-drop database contrader2;
-create database contrader2;
-create table contrader2.users (username varchar(50), password varchar(50));
-insert into contrader2.users (username, password) values ('pippo', 'paperino');
-insert into contrader2.users (username, password) values ('ciccio', 'bello');
-create table contrader2.gomme (model varchar(50), manufacturer varchar(50), price float);
-insert into contrader2.gomme (model, manufacturer, price) values ('Trelleborg-B50', 'Trelleborg', 321.13);
-insert into contrader2.gomme (model, manufacturer, price) values ('SuperG-876 ', 'Super Gomme', 213.176);
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sytdb
+-- ------------------------------------------------------
+-- Server version	5.5.54-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `pubblicazione`
+--
+
+DROP TABLE IF EXISTS `pubblicazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pubblicazione` (
+  `id_pubblicazione` int(11) NOT NULL AUTO_INCREMENT,
+  `pubblicazione` varchar(45) NOT NULL,
+  `id_utente` int(11) NOT NULL,
+  PRIMARY KEY (`id_pubblicazione`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pubblicazione`
+--
+
+LOCK TABLES `pubblicazione` WRITE;
+/*!40000 ALTER TABLE `pubblicazione` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pubblicazione` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-11 10:03:25
--- src/main/dao/LoginDAO.java
+++ src/main/dao/LoginDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class LoginDAO {
 
-    private final String QUERY_LOGIN = "select * from users where username = ? and password = ?";
+    private final String QUERY_LOGIN = "select * from utente where username = ? and password = ?";
 
     public boolean login (String username, String password) {
 
