899f201aec4e9accb4425bc0da12e2a39356adad
PeppeScagliola <g.scagliola@contrader.it>
2018-07-23 17:23:26 +0200

    Progetto base

---
+++ src/main/java/SYT/SYTSpring/SytSpringApplication.java
@@ -0,0 +1,12 @@
+package SYT.SYTSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SytSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SytSpringApplication.class, args);
+	}
+}
