7a76726503473d23c21a0464f2363312083c607c
unknown <s216551studenti.polito.it>
2018-07-25 15:34:04 +0200

    Emiliano: visualizza classifica sistema da home giudice

--- src/main/java/com/sytSpring/dao/ClassificaSistemaRepository.java
+++ src/main/java/com/sytSpring/dao/ClassificaSistemaRepository.java
@@ -2,10 +2,33 @@ package com.sytSpring.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.sytSpring.model.Registrazione;
 
-public interface ClassificaSistemaRepository {
-
-	List<Registrazione> getClassifica();
+@Repository
+@Transactional
+public interface ClassificaSistemaRepository extends CrudRepository<Registrazione, Long>{
+	
+	
+	
+	@Modifying
+	@Query("SELECT  r FROM Registrazione r , VotazioneSistema v \r\n" + 
+			"WHERE r.idRegistrazione=v.idregistrazione \r\n" + 
+			"AND data < '2018-07-25' \r\n" + 
+			"AND data > '2018-07-20' \r\n" + 
+			"ORDER BY fedelta DESC")
+	 public List<Registrazione> selectTop10();
 
 }
+
+/*"SELECT  r FROM Registrazione r , VotazioneSistema v \r\n" + 
+			"WHERE r.idregistrazione=v.idregistrazione \r\n" + 
+			"AND data < '2018-07-25'\r\n" + 
+			"AND data > '2018-07-20'\r\n" + 
+			"ORDER BY fedelta DESC LIMIT 10"*/
