ddb99864bd2dc5e96bbe66ae8b585bd27fc09b7e
PaoloMariaDeMasi <p.demasi@contrader.it>
2018-08-03 16:29:04 +0200

    Home giudice e contatti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '../../node_modules/@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeGiudiceComponent } from './components/home-giudice/home-giudice.component';
 import { HomeCantanteComponent } from './components/home-cantante/home-cantante.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'homeGiudice', component: HomeGiudiceComponent},
-    {path: 'homeCantante', component: HomeCantanteComponent}
+    {path: 'homeCantante', component: HomeCantanteComponent},
+    {path: 'contact', component: ContactComponent},
+    
+    
   ];
 
   @NgModule({
