fd9cce100eae65288fca96fb6fe2847129a62a1f
PeppeScagliola <g.scagliola@contrader.it>
2018-08-24 10:02:18 +0200

    Edit registrazione model

--- src/app/models/registrazione.model.ts
+++ src/app/models/registrazione.model.ts
@@ -2,11 +2,13 @@ export class Registrazione {
     idRegistrazione: number;
     dataCreazione: string;
     idCanzone: number;
+    idUtente: number;
     
       
-    constructor(idRegistrazione: number, dataCreazione: string, idCanzone: number) {
+    constructor(idRegistrazione: number, dataCreazione: string, idCanzone: number, idUtente: number) {
         this.idRegistrazione = idRegistrazione;
         this.dataCreazione = dataCreazione;
         this.idCanzone = idCanzone;
+        this.idUtente = idUtente;
       }
   }
